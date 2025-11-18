999178d9793b3fff7c00395b5ae58b5faef21fcd
Fabricio Cervantes <fabriciocervantesmendoza@gmail.com>
2025-11-18 17:07:26 -0500

    feat: Inclusion de Front en Angular 18 y Back en SpringBoot 3.5

---
+++ back-reporte-deploy-app/src/main/java/com/back/back_reporte_deploy_app/BackReporteDeployAppApplication.java
@@ -0,0 +1,13 @@
+package com.back.back_reporte_deploy_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackReporteDeployAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackReporteDeployAppApplication.class, args);
+	}
+
+}
