ee45c0b043a9ce25a370479ae07394567adcfa80
Fabricio Cervantes <fabriciocervantesmendoza@gmail.com>
2025-11-24 16:57:48 -0500

    feat: Add Entity Responsable

--- back-reporte-deploy-app/src/main/java/com/back/back_reporte_deploy_app/entity/Responsable.java
+++ back-reporte-deploy-app/src/main/java/com/back/back_reporte_deploy_app/entity/Responsable.java
@@ -1,5 +1,22 @@
 package com.back.back_reporte_deploy_app.entity;
 
-public class Responsable {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
 
+@Entity
+@Data
+public class Responsable {
+    @Id
+    private Integer id;
+
+    @Column(length = 40)
+    private String nom;
+
+    @Column(length = 40)
+    private String ape; 
+    
+    @Column(length = 40)
+    private String cargo;
 }
