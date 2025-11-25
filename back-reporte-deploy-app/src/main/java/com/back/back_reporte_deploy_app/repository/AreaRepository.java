2c6196b3a96cc492c816df797b365574e754c36c
Fabricio Cervantes <fabriciocervantesmendoza@gmail.com>
2025-11-24 23:25:01 -0500

    feat: Add repository para Area

---
+++ back-reporte-deploy-app/src/main/java/com/back/back_reporte_deploy_app/repository/AreaRepository.java
@@ -0,0 +1,9 @@
+package com.back.back_reporte_deploy_app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.back.back_reporte_deploy_app.entity.Area;
+
+public interface AreaRepository extends JpaRepository<Area, Long> {
+
+}
