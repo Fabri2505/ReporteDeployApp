91b497ad6cb3cd39943ac8dff7ed64d7bf02312a
Fabricio Cervantes <fabriciocervantesmendoza@gmail.com>
2025-11-27 23:32:06 -0500

    feat: Add componentes compartidos

---
+++ front-reporte-deploy-app/src/app/share/layout-main/layout-main.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-main',
+  imports: [],
+  templateUrl: './layout-main.component.html',
+  styleUrl: './layout-main.component.css'
+})
+export class LayoutMainComponent {
+
+}
