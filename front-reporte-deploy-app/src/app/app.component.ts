e0af812ded9f4851701c896ea44bd743b1bf2280
Fabricio Cervantes <fabriciocervantesmendoza@gmail.com>
2025-11-27 23:29:18 -0500

    feat: Configurado primng con tailwind css 4 mas manejar el estilo para noir

--- front-reporte-deploy-app/src/app/app.component.ts
+++ front-reporte-deploy-app/src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Dialog } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Dialog, ButtonModule, InputTextModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'front-reporte-deploy-app';
+
+  visible: boolean = false;
+
+  showDialog() {
+    this.visible = true;
+  }
+
 }
