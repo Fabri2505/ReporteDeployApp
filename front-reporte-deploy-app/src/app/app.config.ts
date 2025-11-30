ebb8c752afebc528c45db094a3691384815872c1
Fabricio Cervantes <fabriciocervantesmendoza@gmail.com>
2025-11-29 22:22:26 -0500

    feat: Cambiar primeng por spartanng

--- front-reporte-deploy-app/src/app/app.config.ts
+++ front-reporte-deploy-app/src/app/app.config.ts
@@ -1,22 +1,11 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { providePrimeNG } from 'primeng/config';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
-
 import { routes } from './app.routes';
-import { MyPreset } from './mypreset';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideAnimationsAsync(),
-    providePrimeNG({
-      theme: {
-        preset: MyPreset
-      }
-    }),
     provideRouter(routes)
   ]
 };
--- front-reporte-deploy-app/src/app/app.ts
+++ front-reporte-deploy-app/src/app/app.ts
@@ -1,22 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
-import { CardModule } from 'primeng/card';
+import { HlmButtonImports } from '@spartan-ng/helm/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, InputTextModule, CardModule],
+  imports: [RouterOutlet, HlmButtonImports],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('front-reporte-deploy-app');
-
-  visible: boolean = false;
-
-  showDialog() {
-    this.visible = true;
-  }
 
 }
