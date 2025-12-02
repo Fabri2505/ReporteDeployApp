b42a4c719617384159edd80dbab85a3e0a13d1e0
Fabricio Cervantes <fabriciocervantesmendoza@gmail.com>
2025-12-01 23:26:25 -0500

    feat: Add modo oscuro y componente de pagina no encontrada

--- front-reporte-deploy-app/src/app/app.routes.ts
+++ front-reporte-deploy-app/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadChildren: () => import('./report/reporte.routes').then(m => m.routes)
+    },
+    {
+        path: 'not-found',
+        loadComponent: () => import('./share/not-found/not-found').then(m => m.NotFound)
+    },
+    {
+        path: '**',
+        redirectTo: 'not-found'
+    }
+];
